be5e7c57ebdc19bb9e97299ba4516f3d9d8c8e7a
Mark Manders <mmjmanders@gmail.com>
2026-05-01 12:24:57 +0200

    Feature/add fonts (#2)
    * Update `pnpm-lock.yaml` to add `@fontsource-variable/geist` dependency and optimize formatting.
    
    * Update `pnpm-lock.yaml` to add FontAwesome and `geist-mono` dependencies and optimize formatting
    
    * Update `pnpm-lock.yaml` to adjust dependency overrides for `serialize-javascript` and clean up formatting.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCodeMerge, faSquareBinary } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import './main.css'
+
+library.add(faCodeMerge, faSquareBinary)
 
 createApp(App).mount('#app')
