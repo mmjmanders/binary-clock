453a9a7c4f398516e4a4aacf9a4edf75f85659c8
Mark Manders <mmjmanders@gmail.com>
2026-05-01 14:04:03 +0200

    Refactor App component tests: add `beforeEach` for setup, split assertions into separate test cases

--- src/__tests__/App.cy.ts
+++ src/__tests__/App.cy.ts
@@ -1,9 +1,15 @@
 import App from '../App.vue'
 
 describe('App', () => {
-  it('should mount and render properly', () => {
+  beforeEach(() => {
     cy.mount(App)
+  })
+
+  it('should have correct heading', () => {
     cy.get('h1').should('have.text', 'Binary Clock')
+  })
+
+  it('should have correct version', () => {
     cy.get('footer').should('contain.text', 'component')
   })
 })
