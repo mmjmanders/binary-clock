62a14b4fd54a5e006d97f2bdc25d03e9debb4994
Mark Manders <mmjmanders@gmail.com>
2026-05-04 08:44:45 +0200

    Refactor BinaryClock component: add ClockItem subcomponent and update layout styles for improved reusability and responsiveness

--- src/components/BinaryClock.vue
+++ src/components/BinaryClock.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
+import ClockItem from './ClockItem.vue'
 import { useClock } from '@/composables/clock'
 import { computed } from 'vue'
 import dayjs from 'dayjs'
 
 const { time } = useClock()
 const displayTime = computed(() => dayjs(time.value).format('HH:mm:ss'))
+const hours = computed(() => time.value.getHours() % 12 || 12)
+const minutes = computed(() => time.value.getMinutes())
+const seconds = computed(() => time.value.getSeconds())
 </script>
 
 <template>
   <div class="main-content">
     <div class="clock">
-      <svg viewBox="0 0 1600 900" preserveAspectRatio="xMidYMid meet">
-        <rect width="1600" height="900" />
+      <svg viewBox="0 0 1200 900" preserveAspectRatio="xMidYMid meet">
+        <ClockItem :leftDigits="2" :rightDigits="4" :value="hours" />
+        <ClockItem :leftDigits="3" :rightDigits="4" :offset="400" :value="minutes" />
+        <ClockItem :leftDigits="3" :rightDigits="4" :offset="800" :value="seconds" />
       </svg>
     </div>
     <div class="display-time">{{ displayTime }}</div>
---
+++ src/components/ClockItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    leftDigits: number
+    rightDigits: number
+    value: number
+    offset?: number
+  }>(),
+  {
+    offset: 0,
+  },
+)
+
+const left = computed(() => Math.floor(props.value / 10))
+const right = computed(() => props.value % 10)
+</script>
+
+<template>
+  <g :transform="`translate(${props.offset}, 0)`">
+    <text>{{ left }}</text>
+    <text>{{ right }}</text>
+  </g>
+</template>
+
+<style scoped></style>
