20b62169d8b2cfc9c33a918d6c6a137fef8e1381
Mark Manders <mmjmanders@gmail.com>
2026-05-04 12:28:41 +0200

    Update e2e tests: add new checks for clock element and dots; set 60-minute timeout for workflows

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,3 +1,5 @@
+// import dayjs from 'dayjs'
+
 describe('App', () => {
   it('should have correct title', () => {
     cy.title().should('equal', 'Binary Clock')
@@ -10,4 +12,22 @@ describe('App', () => {
   it('should have correct version', () => {
     cy.get('footer').should('contain.text', 'e2e')
   })
+
+  it('should have clock element', () => {
+    cy.get('.clock > svg').should('be.visible')
+  })
+
+  it('should have 20 dots', () => {
+    cy.get('circle').should('have.length', 20)
+  })
+
+  /* Parameterized tests */
+  /*Array.from({ length: 5 })
+    .map((_, i) => dayjs(new Date('2026-05-04T12:23:00.000+02:00')).add(i, 'second').toDate())
+    .forEach((date) => {
+      it(`should pass for ${date.toISOString()}`, () => {
+        cy.clock(date)
+        cy.visit('/')
+      })
+    })*/
 })
