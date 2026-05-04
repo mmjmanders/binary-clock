74a3928d6c1bf56a98d3faf86ccd75919f2c90d3
Mark Manders <mmjmanders@gmail.com>
2026-05-04 14:24:50 +0200

    Refactor e2e tests: extract `numOnForDate` helper function for reusable bit count calculation and simplify test logic

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,21 @@
 import dayjs from 'dayjs'
 
+const countBits = (n: number) =>
+  n
+    .toString(2)
+    .split('')
+    .filter((b) => b === '1').length
+
+const numOnForDate = (d: Date) =>
+  [
+    Math.floor(d.getHours() / 10),
+    d.getHours() % 10,
+    Math.floor(d.getMinutes() / 10),
+    d.getMinutes() % 10,
+    Math.floor(d.getSeconds() / 10),
+    d.getSeconds() % 10,
+  ].reduce((acc, v) => acc + countBits(v), 0)
+
 describe('App', () => {
   it('should have correct title', () => {
     cy.title().should('equal', 'Binary Clock')
@@ -23,70 +39,43 @@ describe('App', () => {
 
   /* Parameterized tests */
   describe('hours', () => {
-    afterEach(() => cy.clock().invoke('restore'))
+    afterEach(() => cy.clock().then((clock) => clock.restore()))
 
     Array.from({ length: 24 })
       .map((_, i) => dayjs(new Date('2026-05-04T00:00:00.000+02:00')).add(i, 'hours'))
       .forEach((date) => {
         it(`should pass for hours of ${date.format('HH:mm:ss')}`, () => {
           cy.clock(date.toDate())
-          const numOn = [Math.floor(date.get('hours') / 10), date.get('hours') % 10].reduce(
-            (acc, value) =>
-              acc +
-              value
-                .toString(2)
-                .split('')
-                .filter((n) => n === '1').length,
-            0,
-          )
           cy.visit('/')
-          cy.get('.is-on').should('have.length', numOn)
+          cy.get('.is-on').should('have.length', numOnForDate(date.toDate()))
         })
       })
   })
 
   describe('minutes', () => {
-    afterEach(() => cy.clock().invoke('restore'))
+    afterEach(() => cy.clock().then((clock) => clock.restore()))
 
     Array.from({ length: 60 })
       .map((_, i) => dayjs(new Date('2026-05-04T00:00:00.000+02:00')).add(i, 'minutes'))
       .forEach((date) => {
         it(`should pass for minutes of ${date.format('HH:mm:ss')}`, () => {
           cy.clock(date.toDate())
-          const numOn = [Math.floor(date.get('minutes') / 10), date.get('minutes') % 10].reduce(
-            (acc, value) =>
-              acc +
-              value
-                .toString(2)
-                .split('')
-                .filter((n) => n === '1').length,
-            0,
-          )
           cy.visit('/')
-          cy.get('.is-on').should('have.length', numOn)
+          cy.get('.is-on').should('have.length', numOnForDate(date.toDate()))
         })
       })
   })
 
   describe('seconds', () => {
-    afterEach(() => cy.clock().invoke('restore'))
+    afterEach(() => cy.clock().then((clock) => clock.restore()))
 
     Array.from({ length: 60 })
       .map((_, i) => dayjs(new Date('2026-05-04T00:00:00.000+02:00')).add(i, 'seconds'))
       .forEach((date) => {
         it(`should pass for seconds of ${date.format('HH:mm:ss')}`, () => {
           cy.clock(date.toDate())
-          const numOn = [Math.floor(date.get('seconds') / 10), date.get('seconds') % 10].reduce(
-            (acc, value) =>
-              acc +
-              value
-                .toString(2)
-                .split('')
-                .filter((n) => n === '1').length,
-            0,
-          )
           cy.visit('/')
-          cy.get('.is-on').should('have.length', numOn)
+          cy.get('.is-on').should('have.length', numOnForDate(date.toDate()))
         })
       })
   })
