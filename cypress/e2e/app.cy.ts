33336f44a37f7dd2c7ce269f60999a8fe78c7cfe
Mark Manders <mmjmanders@gmail.com>
2026-05-04 13:22:56 +0200

    Update e2e tests: enable dynamic parameterized tests for all seconds in a day and validate `is-on` elements count

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,4 +1,4 @@
-// import dayjs from 'dayjs'
+import dayjs from 'dayjs'
 
 describe('App', () => {
   it('should have correct title', () => {
@@ -22,12 +22,33 @@ describe('App', () => {
   })
 
   /* Parameterized tests */
-  /*Array.from({ length: 5 })
-    .map((_, i) => dayjs(new Date('2026-05-04T12:23:00.000+02:00')).add(i, 'second').toDate())
+  Array.from({ length: 24 * 60 * 60 })
+    .map((_, i) => dayjs(new Date('2026-05-04T12:23:00.000+02:00')).add(i, 'second'))
     .forEach((date) => {
-      it(`should pass for ${date.toISOString()}`, () => {
-        cy.clock(date)
+      it(`should pass for ${date.format('HH:mm:ss')}`, () => {
+        cy.clock(date.toDate())
+        const [hoursLeft, hoursRight] = [Math.floor(date.get('hours') / 10), date.get('hours') % 10]
+        const [minutesLeft, minutesRight] = [
+          Math.floor(date.get('minutes') / 10),
+          date.get('minutes') % 10,
+        ]
+        const [secondsLeft, secondsRight] = [
+          Math.floor(date.get('seconds') / 10),
+          date.get('seconds') % 10,
+        ]
+        const numOn = [
+          hoursLeft,
+          hoursRight,
+          minutesLeft,
+          minutesRight,
+          secondsLeft,
+          secondsRight,
+        ].reduce(
+          (acc, value) => acc + value.toString(2).split('').map(Number).filter(Boolean).length,
+          0,
+        )
         cy.visit('/')
+        cy.get('.is-on').should('have.length', numOn)
       })
-    })*/
+    })
 })
